36854c6511226e8fc189aed7b37f29145ebb5440
julinotmonth <akhmdjzl17@gmail.com>
2026-01-29 18:34:04 +0700

    Describe the changes you made

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+  origin: process.env.FRONTEND_URL || ['https://testing-five-omega-90.vercel.app/', 'http://localhost:5173', 'http://127.0.0.1:5173'],
   credentials: true
 }));
 app.use(express.json());
